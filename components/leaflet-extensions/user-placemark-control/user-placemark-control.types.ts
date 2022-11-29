96affa3a68287b50b4823e35a1fcd22de033bef8
hiba9201 <pelevinaa@yandex-team.ru>
2022-11-29 16:33:05 +0500

    fix animation

--- components/leaflet-extensions/user-placemark-control/user-placemark-control.types.ts
+++ components/leaflet-extensions/user-placemark-control/user-placemark-control.types.ts
@@ -3,8 +3,3 @@ export enum UserPlacemarkControlState {
     Pan = 'pan',
     Centred = 'centred',
 }
-
-export type UserPlacemarkIcons = {
-    buttonIcon: HTMLImageElement;
-    buttonIconCentered: HTMLImageElement;
-};
