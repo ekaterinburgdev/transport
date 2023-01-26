05a6f6166466bf820b0020d0a40482ca9cf10018
Lexeria <avp-avp-13@mail.ru>
2023-01-26 05:38:06 +0500

    added logic for working with sidebar

---
+++ hooks/useDisablePropagation.ts
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+import L from 'leaflet';
+
+export const useDisablePropagation = (ref: React.MutableRefObject<HTMLElement>) => {
+    useEffect(() => {
+        if (ref?.current) {
+            L.DomEvent.disableClickPropagation(ref.current);
+            L.DomEvent.disableScrollPropagation(ref.current);
+        }
+    }, [ref]);
+};
