b1c0c8072dbbc7c0f69e44205c0828b4b7afa12f
Lexeria <avp-avp-13@mail.ru>
2023-01-26 05:41:37 +0500

    added a welcome message

--- components/Map/MainContainer/MapMainContainer.tsx
+++ components/Map/MainContainer/MapMainContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import L from 'leaflet';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import classNames from 'classnames/bind';
@@ -7,11 +7,14 @@ import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
 import iconUrl from 'leaflet/dist/images/marker-icon.png';
 import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
 
+import { sidebarService } from 'services/sidebar/sidebar';
+
 import { COORDS_EKATERINBURG } from 'common/constants/coords';
 
 import { MapLocation } from 'components/Map/Location/MapLocation';
 import { MapTransport } from 'components/Map/Transport/MapTransport';
 import { MapZoomControl } from 'components/Map/ZoomControl/MapZoomControl';
+import { MapWelcomeMessage } from 'components/Map/WelcomeMessage/MapWelcomeMessage';
 
 import styles from './MapMainContainer.module.css';
 import 'leaflet/dist/leaflet.css';
@@ -21,6 +24,8 @@ const cn = classNames.bind(styles);
 function MapMainContainer() {
     const position: [number, number] = COORDS_EKATERINBURG;
 
+    const [sidebar, setSidebar] = useState<React.ReactElement>(null);
+
     useEffect(() => {
         (async function init() {
             L.Icon.Default.mergeOptions({
@@ -29,6 +34,8 @@ function MapMainContainer() {
                 shadowUrl: shadowUrl.src,
             });
         }());
+        sidebarService.setSidebar = setSidebar;
+        sidebarService.open(<MapWelcomeMessage />);
     }, []);
 
     return (
@@ -49,6 +56,8 @@ function MapMainContainer() {
             <MapZoomControl position="bottomright" />
 
             <MapTransport />
+
+            {sidebar}
         </MapContainer>
     );
 }
