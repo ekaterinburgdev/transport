f66748121ff317df66fab1df0342040ece9a93cc
Lexeria <avp-avp-13@mail.ru>
2023-01-26 05:40:24 +0500

    implemented basic UI shared components

---
+++ components/UI/Typography/Typography.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+
+import styles from './Typography.module.css';
+
+export type TypographyProps = {
+    variant: 'h1' | 'h2' | 'h3' | 'caption';
+    children: React.ReactNode;
+    className?: string;
+};
+
+const cn = classNames.bind(styles);
+
+export function Typography({ variant, children, className }: TypographyProps) {
+    switch (variant) {
+        case 'h1':
+            return <h1 className={cn(styles.h1, className)}>{children}</h1>;
+        case 'h2':
+            return <h2 className={cn(styles.h2, className)}>{children}</h2>;
+        case 'h3':
+            return <h3 className={cn(styles.h3, className)}>{children}</h3>;
+        case 'caption':
+            return <p className={cn(styles.caption, className)}>{children}</p>;
+        default:
+            return null;
+    }
+}
+
+Typography.defaultProps = {
+    className: '',
+};
