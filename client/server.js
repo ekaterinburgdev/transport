584a82c7b1b8dc844de033bca336462efb4395db
hiba9201 <pelevinaa@yandex-team.ru>
2023-02-25 18:40:16 +0500

    fix deps

--- client/server.js
+++ client/server.js
@@ -17,15 +17,11 @@ const apiPaths = {
     },
 };
 
-const isDevelopment = process.env.NODE_ENV !== 'production';
-
 app.prepare()
     .then(() => {
         const server = express();
 
-        if (isDevelopment) {
-            server.use('/api', createProxyMiddleware(apiPaths['/api']));
-        }
+        server.use('/api', createProxyMiddleware(apiPaths['/api']));
 
         server.all('/*', (req, res) => {
             return handle(req, res);
