3a4b4c25b2ba02343c70507c1bdff114983f7a5b
Alexander Chabin <nibach@ya.ru>
2023-04-08 23:22:54 +0500

    Check updates

--- api/router/masstrans.ts
+++ api/router/masstrans.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 import { masstransController } from '../controller/masstrans/masstrans';
 
@@ -7,3 +7,7 @@ export const masstransRouter = express.Router();
 masstransRouter.get('/all-units', masstransController.getAllUnits);
 masstransRouter.get('/:unit', masstransController.getUnit);
 masstransRouter.get('/route/:id', masstransController.getRoute);
+
+masstransRouter.get('/one', async function (_: Request, res: Response) {
+    res.json({ data: 'test' });
+})
