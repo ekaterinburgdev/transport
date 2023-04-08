7737d94c36ff5547d97dc72ab4b3a945f95d6955
Alexander Chabin <nibach@ya.ru>
2023-04-09 00:06:48 +0500

    Remove test routes

--- api/router/index.ts
+++ api/router/index.ts
@@ -1,11 +1,7 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 
 import { masstransRouter } from './masstrans';
 
 export const router = express.Router();
 
 router.use('/masstrans', masstransRouter);
-
-router.get('/one', function (_: Request, res: Response) {
-    res.json({ data: 'test' });
-})
--- api/router/masstrans.ts
+++ api/router/masstrans.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 
 import { masstransController } from '../controller/masstrans/masstrans';
 
