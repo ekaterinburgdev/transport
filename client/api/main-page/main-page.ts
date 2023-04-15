5bcb2a6c89fab12eff39d0998d3df2529a290b5c
Alexander Chabin <nibach@ya.ru>
2023-04-15 16:36:34 +0500

    Add temporary fixtures

--- client/api/main-page/main-page.ts
+++ client/api/main-page/main-page.ts
@@ -1,12 +1,20 @@
 import { fetchApi } from 'api/utils/fetch';
 
+const IS_TEST = process.env.NODE_ENV !== 'production';
+
 export const MainPageApi = {
-    getCards: () =>
-        fetchApi('https://transport-cms.ekaterinburg.io/api/cards/?populate=backgroundImage', {
-            dataField: 'data',
-        }),
-    getMarqueeItems: () =>
-        fetchApi('https://transport-cms.ekaterinburg.io/api/marquees', { dataField: 'data' }),
-    getNotifications: () =>
-        fetchApi('https://transport-cms.ekaterinburg.io/api/notifications', { dataField: 'data' }),
+    getCards: () => {
+        if (IS_TEST) return require('../../__fixtures__/cards.json');
+        return fetchApi('https://transport-cms.ekaterinburg.io/api/cards/?populate=backgroundImage', { dataField: 'data' });
+    },
+
+    getMarqueeItems: () => {
+        if (IS_TEST) return require('../../__fixtures__/marquee-items.json');
+        return fetchApi('https://transport-cms.ekaterinburg.io/api/marquees', { dataField: 'data' });
+    },
+
+    getNotifications: () => {
+        if (IS_TEST) return require('../../__fixtures__/notifications.json');
+        return fetchApi('https://transport-cms.ekaterinburg.io/api/notifications', { dataField: 'data' });
+    }
 };
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,12 +4,12 @@ import { MainPageApi } from 'api/main-page/main-page';
 import { MainPage } from 'components/MainPage/MainPage';
 
 export default function Home(props) {
+
     return (
         <>
             <Head>
                 <title>Транспорт Екатеринбурга</title>
             </Head>
-
             <MainPage {...props} />
         </>
     );
