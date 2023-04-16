8c0904104d771788e4bb13c110f876befb07c31a
kihtenko <kihtenko@tochka.com>
2023-04-16 17:54:33 +0500

    Fix underline of Card Title

--- client/components/MainPage/Card/Card.tsx
+++ client/components/MainPage/Card/Card.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import classNames from 'classnames/bind';
-
 import { CardProps } from './Card.types';
 
 import styles from './Card.module.css';
 
 const cn = classNames.bind(styles);
+
+const hexToRgb = (hex) => {
+    const [r, g, b] = hex.match(/\w\w/g).map(x => parseInt(x, 16));
+    return `${r}, ${g}, ${b}`;
+};
 
 export function Card({
     title,
@@ -20,30 +24,31 @@ export function Card({
     subtitle,
     subtitleColor,
 }: CardProps) {
+
     return (
         <a
             className={cn(styles.Card)}
             href={url}
             style={{
-                backgroundColor: backgroundColor || '#fff',
-                backgroundImage: `url(https://transport-cms.ekaterinburg.io${backgroundImage})`,
-            }}
+                "--CardTitleColor": titleColor,
+                "--CardTitleBgrColor": titleBackground,
+                "--CardTitleUnderlineColor": hexToRgb(titleColor),
+                "--CardBgrColor": backgroundColor,
+                "--CardBgrImage": `url(https://transport-cms.ekaterinburg.io${backgroundImage})`,
+                "--CardSubtitleColor": subtitleColor,
+                "--CardButtonColor": buttonColor,
+                "--CardButtonBgrColor": buttonBackground,
+            } as React.CSSProperties}
         >
-            <div
-                className={cn(styles.CardTitle)}
-                style={{ backgroundColor: titleBackground, color: titleColor }}
-            >
+            <div className={cn(styles.CardTitle, { [styles.CardTitle_Bg]: Boolean(titleBackground) })}>
                 {title}
             </div>
 
-            <div className={cn(styles.CardSubtitle)} style={{ color: subtitleColor }}>
+            <div className={cn(styles.CardSubtitle)}>
                 {subtitle}
             </div>
 
-            <div
-                className={cn(styles.CardButton)}
-                style={{ backgroundColor: buttonBackground, color: buttonColor }}
-            >
+            <div className={cn(styles.CardButton)}>
                 {button}
             </div>
         </a>
