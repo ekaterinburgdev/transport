be9af7947c6813eb9c55e3b3cda6894361721e72
hiba9201 <pelevina.2000@mail.ru>
2023-06-12 02:03:00 +0500

    remove errors check

--- client/public/sw.js
+++ client/public/sw.js
@@ -12,7 +12,7 @@ self.addEventListener('fetch', (event) => {
     console.log(`Request of ${event.request.url}`, event.request.cache);
 
     if (event.request.cache === 'no-cache' || event.request.cache === 'no-store') {
-        event.respondWith(fetch(event.request).catch(() => ({})));
+        event.respondWith(fetch(event.request));
 
         return;
     }
@@ -21,7 +21,7 @@ self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches
             .match(event.request)
-            .then((cached) => cached || fetch(event.request).catch(() => ({})))
+            .then((cached) => cached || fetch(event.request))
             .then((response) => cache(event.request, response).then(() => response)),
     );
     event.waitUntil(update(event.request));
