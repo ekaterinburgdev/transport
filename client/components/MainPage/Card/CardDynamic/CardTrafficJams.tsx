ad8757a88ec81983540e7423fc502af5ab064ebb
pornoiya <lactnovpidor@gmail.com>
2023-07-15 19:11:58 +0400

    Automatic color of light by traffic score

--- client/components/MainPage/Card/CardDynamic/CardTrafficJams.tsx
+++ client/components/MainPage/Card/CardDynamic/CardTrafficJams.tsx
@@ -3,16 +3,24 @@ import classNames from 'classnames/bind';
 import styles from './CardTrafficJams.module.css';
 
 const cn = classNames.bind(styles);
-
+const getTrafficLightByScore = (score: any) => {
+    const scoreNumber = parseInt(score?.toString());
+    if (scoreNumber < 4) {
+        return styles.CardTrafficLight_Green;
+    } else if (scoreNumber < 7) {
+        return styles.CardTrafficLight_Yellow;
+    } else {
+        return styles.CardTrafficLight_Red;
+    }
+}
 export function CardTrafficJams({ score }) {
+
     return (
         <div className={styles.CardTrafficLight_Container}>
             <div className={cn(styles.CardTrafficLight_Border)}>
             </div>
-            <div className={cn(styles.CardTrafficJams)}>
-                <div className={cn(styles.CardTrafficLight)}>
-                    {score}
-                </div>
+            <div className={cn(styles.CardTrafficLight, getTrafficLightByScore(score))}>
+                {score}
             </div>
         </div>
     )
