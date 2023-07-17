7e9e17e509f7f43edfb7946acf6c25c102b4afb6
pornoiya <49945883+pornoiya@users.noreply.github.com>
2023-07-17 06:59:34 +0400

    Main page/dynamic cards (#68)
    * Bottom titles
    
    * traffic light draft
    
    * Automatic color of light by traffic score
    
    * Traffic light svg
    
    * refactor

--- client/components/MainPage/Card/CardDynamic/CardTrafficJams.tsx
+++ client/components/MainPage/Card/CardDynamic/CardTrafficJams.tsx
@@ -1,15 +1,25 @@
 import classNames from 'classnames/bind';
 
 import styles from './CardTrafficJams.module.css';
+import React from "react";
 
 const cn = classNames.bind(styles);
-
+const getTrafficLightByScore = (score: any) => {
+    const scoreNumber = parseInt(score?.toString());
+    if (scoreNumber < 4) {
+        return styles.CardTrafficLight_Green;
+    } else if (scoreNumber < 7) {
+        return styles.CardTrafficLight_Yellow;
+    } else {
+        return styles.CardTrafficLight_Red;
+    }
+}
 export function CardTrafficJams({ score }) {
     return (
-        <>
-            <div className={cn(styles.CardTrafficJams)}>
+        <div className={cn(styles.CardTrafficLight__Container)}>
+            <div className={cn(styles.CardTrafficLight, getTrafficLightByScore(score))}>
                 {score}
             </div>
-        </>
+        </div>
     )
 }
