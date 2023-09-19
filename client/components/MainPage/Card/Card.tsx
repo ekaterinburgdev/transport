728fedc7dd0ecf7f086308b71290d89ef2bf410b
Alexander Chabin <nibach@ya.ru>
2023-09-19 14:27:05 +0500

    Move card styles classnames to const

--- client/components/MainPage/Card/Card.tsx
+++ client/components/MainPage/Card/Card.tsx
@@ -1,11 +1,18 @@
 import classNames from 'classnames/bind';
 import { CardProps } from './Card.types';
 import t from 'utils/typograph';
-import React from "react";
+import React from 'react';
 
 import { STRAPI_URL } from 'transport-common/strapi/constants';
 
 import styles from './Card.module.css';
+
+const CARD_TYPES_CLASSNAMES = {
+    public: styles.Card_Public,
+    car: styles.Card_Car,
+    other: styles.Card_Other,
+    pedestrian: styles.Card_Pedestrian,
+};
 
 const cn = classNames.bind(styles);
 
@@ -18,37 +25,33 @@ export function Card({
     backgroundImageHover,
     dynamicContent,
     headerCaption,
-    footerCaption
+    footerCaption,
 }: CardProps) {
-    const cardStyle = {
-        "public": styles.Card_Public,
-        "car": styles.Card_Car,
-        "other": styles.Card_Other,
-        "pedestrian": styles.Card_Pedestrian
-    }
-
     return (
         <a
-            className={cn(styles.Card, cardStyle[type])}
+            className={cn(styles.Card, CARD_TYPES_CLASSNAMES[type])}
             href={url}
-            style={{
-                "--CardTitleBgColor": titleBackgroundColor,
-                "--CardBgImage": backgroundImage && `url(${STRAPI_URL}${backgroundImage})`,
-                "--CardBgImageHover": backgroundImageHover && `url(${STRAPI_URL}${backgroundImageHover})`,
-            } as React.CSSProperties}
+            style={
+                {
+                    '--CardTitleBgColor': titleBackgroundColor,
+                    '--CardBgImage': backgroundImage && `url(${STRAPI_URL}${backgroundImage})`,
+                    '--CardBgImageHover':
+                        backgroundImageHover && `url(${STRAPI_URL}${backgroundImageHover})`,
+                } as React.CSSProperties
+            }
         >
-            {title && <div className={cn(styles.CardTitle, { [styles.CardTitle_Bg]: titleBackgroundColor })}>
-                {t(title)}
-            </div>}
-            {headerCaption && <p className={cn(styles.CardHeaderCaption)}>
-                {t(headerCaption)}
-            </p>}
-            {footerCaption && <p className={cn(styles.CardFooterCaption)}>
-                {t(footerCaption)}
-            </p>}
-            {dynamicContent && <div className={cn(styles.CardDynamic)}>
-                {dynamicContent}
-            </div>}
+            {title && (
+                <div
+                    className={cn(styles.CardTitle, {
+                        [styles.CardTitle_Bg]: titleBackgroundColor,
+                    })}
+                >
+                    {t(title)}
+                </div>
+            )}
+            {headerCaption && <p className={cn(styles.CardHeaderCaption)}>{t(headerCaption)}</p>}
+            {footerCaption && <p className={cn(styles.CardFooterCaption)}>{t(footerCaption)}</p>}
+            {dynamicContent && <div className={cn(styles.CardDynamic)}>{dynamicContent}</div>}
         </a>
     );
 }
