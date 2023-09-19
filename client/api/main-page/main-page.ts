aaebb31503068c3847e98526f9095b4fa102e51f
Alexander Chabin <nibach@ya.ru>
2023-09-19 15:44:54 +0500

    Move urls to variables

--- client/api/main-page/main-page.ts
+++ client/api/main-page/main-page.ts
@@ -1,17 +1,18 @@
 import { Unit } from 'transport-common/types/masstrans';
 import { fetchApi } from 'api/utils/fetch';
+import { STRAPI_URL, TRAFFIC_JAMS_URL, TRANSPORT_API_URL } from 'transport-common/strapi/constants';
 
 export const MainPageApi = {
     getCards: async () => {
         return await fetchApi(
-            'https://transport-cms.ekaterinburg.city/api/cards/?populate=backgroundImage,backgroundImageHover',
+            `${STRAPI_URL}/api/cards/?populate=backgroundImage,backgroundImageHover`,
             { dataField: 'data' },
         );
     },
 
     getTrafficJamsCounter: async () => {
         return await fetchApi(
-            'https://ekb-probki.vercel.app/api',
+            TRAFFIC_JAMS_URL,
             { dataField: 'score' },
         );
     },
@@ -19,7 +20,7 @@ export const MainPageApi = {
     getA11yTransportCounters: async () => {
         const countA11yUnits = async (type) => {
             const units = <Unit[]>await fetchApi(
-                `https://transport.ekaterinburg.city/api/masstrans/${type}`,
+                `${TRANSPORT_API_URL}/api/masstrans/${type}`,
                 { dataField: 'data' }
             );
             return units
@@ -35,7 +36,7 @@ export const MainPageApi = {
     },
 
     getMarqueeItems: async () => {
-        return await fetchApi('https://transport-cms.ekaterinburg.city/api/marquees', {
+        return await fetchApi(`${STRAPI_URL}/api/marquees`, {
             dataField: 'data',
         });
     }
