c37bfc513078170f2393409d99797a1aba28a2b7
Alexander Chabin <nibach@ya.ru>
2023-11-13 10:58:49 +0500

    Create hook width `figma-squircle`

---
+++ client/components/MainPage/Card/useCardSmoothCorners.ts
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import type { MutableRefObject } from "react";
+import { getSvgPath } from "figma-squircle";
+
+export function useCardSmoothCorners(cardRef: MutableRefObject<HTMLAnchorElement>) {
+    useEffect(() => {
+        const setCorners = (entry: ResizeObserverEntry) => {
+            const { width, height } = entry.contentRect;
+            const cornerRadius = parseInt(window.getComputedStyle(entry.target).borderRadius, 10);
+            const clipPath = getSvgPath({ width, height, cornerRadius, cornerSmoothing: 0.6 });
+            cardRef.current.style.clipPath = `path('${clipPath}')`;
+        }
+
+        const resizeObserver = new ResizeObserver(([entry]) => {
+            setCorners(entry);
+        });
+
+        resizeObserver.observe(cardRef.current);
+
+        return () => {
+            resizeObserver.disconnect();
+        };
+    }, []);
+};
