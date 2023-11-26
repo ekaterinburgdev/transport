695482a7cebda4d02a2d934235e5ae9cc01a20fd
Alexander Chabin <alex.chabin@gmail.com>
2023-11-26 14:46:02 +0500

    Add smooth corners to `<Card>` (#89)
    * Create hook width `figma-squircle`
    
    * Add hook to Card
    
    * Change value to 0.8
    
    * Fix CardInner size
    
    * Use only one observer. Get value from `--smooth-corners`
    
    * Set `preserveSmoothing`
    
    * Fix `clip-path` sizing
    
    * Use smooth corners only on titles with bgcolor
    
    * Add smooth corners to `<Marquee>`
    
    * Rename `cardRef` to `elRef`
    
    * Remove changes in `MapVehicesSidebar`
    
    * Rename `MarqueeRef`

--- client/components/MainPage/Card/Card.tsx
+++ client/components/MainPage/Card/Card.tsx
@@ -1,9 +1,10 @@
+import { useRef } from 'react';
 import classNames from 'classnames/bind';
-import { CardProps } from './Card.types';
 import t from 'utils/typograph';
-import React from 'react';
-
 import { STRAPI_URL } from 'transport-common/strapi/constants';
+import { useSmoothCorners } from './useSmoothCorners';
+
+import { CardProps } from './Card.types';
 
 import styles from './Card.module.css';
 
@@ -27,10 +28,19 @@ export function Card({
     headerCaption,
     footerCaption,
 }: CardProps) {
+    const cardRef = useRef<HTMLAnchorElement>(null);
+    const cardTitleRef = useRef<HTMLDivElement>(null);
+    useSmoothCorners(cardRef);
+
+    if (titleBackgroundColor) {
+        useSmoothCorners(cardTitleRef);
+    }
+
     return (
         <a
             className={cn(styles.Card, CARD_TYPES_CLASSNAMES[type])}
             href={url}
+            ref={cardRef}
             style={
                 {
                     '--CardTitleBgColor': titleBackgroundColor,
@@ -45,12 +55,23 @@ export function Card({
                     className={cn(styles.CardTitle, {
                         [styles.CardTitle_Bg]: titleBackgroundColor,
                     })}
+                    ref={cardTitleRef}
                 >
                     {t(title)}
                 </div>
             )}
             {headerCaption && <p className={cn(styles.CardHeaderCaption)}>{t(headerCaption)}</p>}
-            {footerCaption && <p className={headerCaption ? cn(styles.CardFooterCaption) : cn(styles.CardFooterCaption_NoSubtitle)}>{t(footerCaption)}</p>}
+            {footerCaption && (
+                <p
+                    className={
+                        headerCaption
+                            ? cn(styles.CardFooterCaption)
+                            : cn(styles.CardFooterCaption_NoSubtitle)
+                    }
+                >
+                    {t(footerCaption)}
+                </p>
+            )}
             {dynamicContent && <div className={cn(styles.CardDynamic)}>{dynamicContent}</div>}
         </a>
     );
---
+++ client/components/MainPage/Card/useSmoothCorners.ts
@@ -0,0 +1,38 @@
+import type { MutableRefObject } from 'react';
+import { useEffect } from 'react';
+import { getSvgPath } from 'figma-squircle';
+
+function setCorners(entry: ResizeObserverEntry) {
+    const element = entry.target as HTMLElement;
+    const styles = window.getComputedStyle(element);
+    const { top, right, bottom, left } = entry.contentRect;
+
+    const clipPath = getSvgPath({
+        width: left + right,
+        height: top + bottom,
+        cornerRadius: parseInt(styles.borderRadius, 10),
+        cornerSmoothing: Number(styles.getPropertyValue('--smooth-corners')),
+    });
+
+    element.style.clipPath = `path('${clipPath}')`;
+}
+
+let smoothCornersObserver = null;
+
+if ('ResizeObserver' in globalThis) {
+    smoothCornersObserver = new ResizeObserver((entries) => {
+        for (const entry of entries) {
+            setCorners(entry);
+        }
+    });
+}
+
+export function useSmoothCorners(elRef: MutableRefObject<HTMLElement>) {
+    useEffect(() => {
+        smoothCornersObserver.observe(elRef.current);
+
+        return () => {
+            smoothCornersObserver.unobserve(elRef.current);
+        };
+    }, []);
+}
