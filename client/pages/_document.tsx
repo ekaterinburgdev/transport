a92fab6abe37fb1076c7ccfb0bd6c673c7e356f0
Alexander Chabin <nibach@ya.ru>
2023-12-02 17:30:51 +0500

    Add Yandex Metrika counter

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -34,6 +34,9 @@ export default function Document() {
             <body>
                 <Main />
                 <NextScript />
+                
+                {/* eslint-disable-next-line react/no-danger */}
+                {process.env.YANDEX_METRIKA && <div dangerouslySetInnerHTML={{ __html: `<script>(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");ym(${process.env.YANDEX_METRIKA}, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true });</script><noscript><div><img src="https://mc.yandex.ru/watch/${process.env.YANDEX_METRIKA}" style="position:absolute; left:-9999px;" alt="" /></div></noscript>` }} />}
             </body>
         </Html>
     );
