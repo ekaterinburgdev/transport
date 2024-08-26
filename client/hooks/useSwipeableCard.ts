51580fbbab72baa388d961e2c1ba8e0b525d9d21
Sasha Chabin <alex.chabin@gmail.com>
2024-08-26 19:46:57 +0500

    Make a `<Modal>` controllable via touch events

--- client/hooks/useSwipeableCard.ts
+++ client/hooks/useSwipeableCard.ts
@@ -1,34 +1,34 @@
 import React, { useState } from 'react';
 
-export enum SidepagePosition {
+export enum CardPosition {
     FullOpen = 'fullOpen',
     HalfOpen = 'halfOpen',
     Hidden = 'hidden',
 }
 
-export const MODAL_POSITIONS = [
-    SidepagePosition.FullOpen,
-    SidepagePosition.HalfOpen,
-    SidepagePosition.Hidden,
+export const CARD_POSITIONS = [
+    CardPosition.FullOpen,
+    CardPosition.HalfOpen,
+    CardPosition.Hidden,
 ];
 export const CHANGE_POSITION_DELTA = 60;
 
-export function useSidepage(
-    startPosition = SidepagePosition.FullOpen,
-): [SidepagePosition, React.TouchEventHandler, React.TouchEventHandler] {
+export function useSwipeableCard(
+    startPosition = CardPosition.FullOpen,
+): [CardPosition, React.TouchEventHandler, React.TouchEventHandler] {
     const [currentPositionIndex, setCurrentPositionIndex] = useState(
-        MODAL_POSITIONS.findIndex((pos) => pos === startPosition),
+        CARD_POSITIONS.findIndex((pos) => pos === startPosition),
     );
     const [currentPosition, setCurrentPosition] = useState(startPosition);
     let dragStartPosition: null | number = null;
     let isDragFinished = false;
 
     function nextPosition(scrollElement?: HTMLElement) {
-        const newPositionIndex = Math.min(currentPositionIndex + 1, MODAL_POSITIONS.length - 1);
+        const newPositionIndex = Math.min(currentPositionIndex + 1, CARD_POSITIONS.length - 1);
         setCurrentPositionIndex(newPositionIndex);
-        setCurrentPosition(MODAL_POSITIONS[newPositionIndex]);
+        setCurrentPosition(CARD_POSITIONS[newPositionIndex]);
 
-        if (scrollElement && newPositionIndex === MODAL_POSITIONS.length - 1) {
+        if (scrollElement && newPositionIndex === CARD_POSITIONS.length - 1) {
             scrollElement.scrollTo({
                 top: 0,
             });
@@ -38,7 +38,7 @@ export function useSidepage(
     function prevPosition() {
         const newPositionIndex = Math.max(currentPositionIndex - 1, 0);
         setCurrentPositionIndex(newPositionIndex);
-        setCurrentPosition(MODAL_POSITIONS[newPositionIndex]);
+        setCurrentPosition(CARD_POSITIONS[newPositionIndex]);
     }
 
     function onDragEnd(event: React.TouchEvent) {
@@ -65,8 +65,8 @@ export function useSidepage(
         if (currentDragPosition - dragStartPosition > CHANGE_POSITION_DELTA) {
             isDragFinished = true;
 
-            const SidepageElement = (event.target as HTMLElement).parentElement;
-            nextPosition(SidepageElement);
+            const cardElement = (event.target as HTMLElement).parentElement;
+            nextPosition(cardElement);
         }
 
         if (currentDragPosition - dragStartPosition < -CHANGE_POSITION_DELTA) {
