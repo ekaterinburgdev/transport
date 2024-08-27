357528ad8e61cae97591e86a0e933f379dbd6f8e
Sasha Chabin <alex.chabin@gmail.com>
2024-08-28 04:34:42 +0500

    Create text macros <Card> for Strapi CMS (#122)

--- client/components/MainPage/Card/Card.types.ts
+++ client/components/MainPage/Card/Card.types.ts
@@ -3,16 +3,15 @@ import React, { ReactElement } from 'react';
 import { TCardSize, TCardType } from 'api/main-page/main-page.types';
 
 export type CardProps = {
-    type: TCardType | undefined;
+    type?: TCardType | undefined;
     title: string;
-    titleBackgroundColor: string;
+    titleBackgroundColor?: string;
     url: string;
-    size: TCardSize | undefined,
-    backgroundImage: string;
-    backgroundImageHover: string;
-    headerCaption: string;
-    footerCaption: string;
-    dynamicContent: ReactElement;
-    key: number;
+    size?: TCardSize | undefined,
+    backgroundImage?: string;
+    backgroundImageHover?: string;
+    headerCaption?: string;
+    footerCaption?: string;
+    dynamicContent?: ReactElement;
     onClick?: (e: React.MouseEvent<HTMLAnchorElement>) => void;
 };
